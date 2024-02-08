e98fdbe9ae47933cb217bd70a07eac74f9346bae
lucas <lucaswtds@gmail.com>
2024-02-08 13:52:23 -0300

    fix: category list request return logic and map

--- frontend/src/components/CategoryList/CategoryList.tsx
+++ frontend/src/components/CategoryList/CategoryList.tsx
@@ -16,16 +16,19 @@ export interface Category {
 export function CategoryList() {
   const { data, isLoading } = useQuery("categories", () => {
     return axios.get("http://localhost:3000/category/").then((response) => {
-      const categoriesRelated: Category[] = response.data.map(
-        (category: Category) => {
+      const categoriesRelated: Category[] = response.data
+        .filter(
+          (category: Category) =>
+            category.parent === "" || category.parent === null
+        )
+        .map((category: Category) => {
           return {
             ...category,
             child: response.data.filter(
               (childCategory: Category) => childCategory.parent === category.id
             ),
           };
-        }
-      );
+        });
       return categoriesRelated;
     });
   });
@@ -37,12 +40,12 @@ export function CategoryList() {
       ) : (
         data?.map((category: Category) => {
           return (
-            <div>
+            <div key={category.id}>
               <h2>{category.name}</h2>
               {category.child?.length ? (
-                category.child.map((childCategory: Category) => {
+                category.child.map((childCategory: Category, index: number) => {
                   return (
-                    <div>
+                    <div key={index}>
                       <h3>{childCategory.name}</h3>
                       {childCategory.products.length ? (
                         <ProductList products={category.products} />
