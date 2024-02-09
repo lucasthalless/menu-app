6fb30bbfccce0ed5ad7bdc397a2277d21d0a65b4
lucas <lucaswtds@gmail.com>
2024-02-09 19:17:22 -0300

    feat: add auth logic to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,21 @@ import "./styles/global.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Products } from "./pages/Products";
 import { Error } from "./pages/Error";
-import { Header } from "./components/Header";
+import { ProductDetails } from "./pages/ProductDetails";
+import { Login } from "./pages/Login/Login";
+import { PrivateRoutes } from "./utils/PrivateRoutes";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="container">
-        <Header />
         <Routes>
-          <Route path="/" element={<Products />} />
-          <Route path="*" element={<Error />} />
+          <Route element={<PrivateRoutes />}>
+            <Route path="/products" index element={<Products />} />
+            <Route path="/product-details/:id" element={<ProductDetails />} />
+            <Route path="*" element={<Error />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </BrowserRouter>
