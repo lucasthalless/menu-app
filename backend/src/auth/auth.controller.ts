45dc0bafc378adf0797b7d2b86f5b784bd15f876
lucas <lucaswtds@gmail.com>
2024-02-09 15:37:22 -0300

    feat: add auth endpoint

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from '@src/auth/auth.service';
+import { SingInDto } from '@src/auth/dto/sing-in.dto';
+
+import { SetMetadata } from '@nestjs/common';
+
+export const IS_PUBLIC_KEY = 'isPublic';
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Public()
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signInDto: SingInDto) {
+    return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+}
